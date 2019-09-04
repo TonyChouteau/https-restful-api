c64a10e0a06dacfd5a29037229d9dec272546d5e
Tony Chouteau <34800257+TonyChouteau@users.noreply.github.com>
2019-09-04 16:52:02 +0200

    add gingonic

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
-import (
-	// "fmt"
-	// "io"
-	"log"
+import {
+	"github.com/gin-gonic/gin"
 	"net/http"
-)
-
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	enableCors(&w)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
-	// fmt.Fprintf(w, "This is an example server.\n")
-	// io.WriteString(w, "This is an example server.\n")
-}
-
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	"log"
 }
 
 func main() {
-	http.HandleFunc("/hello", HelloServer)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	err := http.ListenAndServeTLS(":8081", "/etc/letsencrypt/live/vps.tonychouteau.fr/fullchain.pem", "/etc/letsencrypt/live/vps.tonychouteau.fr/privkey.pem", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
